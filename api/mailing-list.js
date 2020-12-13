a7c609bd4c8f8d10ccf8a8291f62d8cd4c639129
Sam Ternent <sam.ternent@gmail.com>
2020-12-13 23:21:44 +0000

    escaping newlines

--- api/mailing-list.js
+++ api/mailing-list.js
@@ -1,14 +1,11 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 async function writeEmailToSheet(email) {
-  console.log(process.env.GOOGLE_SHEET_ID);
-  console.log(process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL);
-  console.log(process.env.GOOGLE_PRIVATE_KEY);
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   });
 
   const sheet = doc.sheetsByIndex[0];
